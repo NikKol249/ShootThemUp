301a2e71af70b65defcb983e40c085e1ab484788
nohero <nikkol249@gmail.com>
2024-09-05 19:43:43 +0300

    add health pickup functional and rotate pickups

--- Source/ShootThemUp/Public/Components/STUHealthComponent.h
+++ Source/ShootThemUp/Public/Components/STUHealthComponent.h
@@ -34,6 +34,9 @@ public:
 	    return Health / MaxHealth;
     }
 
+	bool TryToAddHealth(float HealthAmount);
+	bool IsHealthFull() const;
+
 protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Health", meta = (ClampMin = "0.0", ClampMax = "1000.0"))
     float MaxHealth = 100.0f;
